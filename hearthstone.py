15c386ffeaa0ed0b3b19b0d8aa3bab800c690cdc
shuhei0916 <shuheiito.stc@gmail.com>
2024-12-08 14:12:09 +0900

    Modify .gitignore

--- hearthstone.py
+++ hearthstone.py
@@ -2,3 +2,21 @@ import pyautogui
 
 def click_play_button(x, y):
     pyautogui.click(x, y)
+    
+def click_play_button_by_image(image_path): 
+    
+    pyautogui.screenshot('data/debug_screenshot.png')
+    
+    location = pyautogui.locateOnScreen(image_path, confidence=0.8)
+    if location: 
+        x, y, width, height = location
+        # ボタンの中央をクリック
+        pyautogui.click(x + width / 2, y + height / 2)
+
+
+def main():
+    click_play_button_by_image('data/play_button.png')
+        
+        
+if __name__ == '__main__':
+    main()

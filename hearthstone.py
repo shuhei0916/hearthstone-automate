917d9c16dacf47c8af2ff45c6216c12a9da6c5b8
shuhei0916 <shuheiito.stc@gmail.com>
2024-12-09 12:54:34 +0900

    Complete to implementation of the matchi-stating function.

--- hearthstone.py
+++ hearthstone.py
@@ -14,13 +14,6 @@ def click_button_by_image(image_path):
         pyautogui.click(x + width / 2, y + height / 2)
 
 
-# def choose_opponent(image_path):
-#     location = pyautogui.locateOnScreen(image_path, confidence=0.8)
-#     if location: 
-#         x, y, width, height = location
-#         # ボタンの中央をクリック
-#         pyautogui.click(x + width / 2, y + height / 2)
-
 def main():
     # マッチの開始処理
     click_button_by_image('data/play_button.png')
@@ -29,8 +22,37 @@ def main():
     time.sleep(1)
     click_button_by_image('data/play_button2.png')
     
+    # マリガン画面まで待機
+    print("Waiting for the confirm button to appear...")
+    while True:
+        try:
+            location = pyautogui.locateOnScreen('data/confirm_button.png', confidence=0.8)
+            if location:
+                break
+        except pyautogui.ImageNotFoundException:
+            pass
+        time.sleep(0.5)  # 短い間隔で確認
+    print("Confirm button appeared!")
     
-        
+    
+    # マリガンせずにコンシード
+    pyautogui.press('esc')
+    time.sleep(0.5)
+    click_button_by_image('data/concede_button.png')
+    time.sleep(10)
+    pyautogui.click(100, 501) # 適当な場所をクリックしてdeck選択画面に戻る
+    
+    # 
+    while True:
+        try:
+            location = pyautogui.locateOnScreen('data/play_button.png', confidence=0.8)
+            if location:
+                break
+        except pyautogui.ImageNotFoundException:
+            pass
+        time.sleep(0.5)  # 短い間隔で確認 
+    
+
         
 if __name__ == '__main__':
     main()

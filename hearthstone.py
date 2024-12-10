f82ad4d84ea3570d15fc3074aa327931884e3dd1
shuhei0916 <shuheiito.stc@gmail.com>
2024-12-10 13:22:13 +0900

    Add play_vs_innkeeper() and Refactor

--- hearthstone.py
+++ hearthstone.py
@@ -27,44 +27,33 @@ def wait_for_image(image_path, timeout=30):
             raise TimeoutError(f'Timeout reached while waiting for {image_path}')
         time.sleep(0.5)
 
-def main():
+def start_match():
     # マッチの開始処理
     click_button_by_image('data/play_button.png')
     time.sleep(1)
     click_button_by_image('data/demon_hunter.png') # select opponent
     time.sleep(1)
     click_button_by_image('data/play_button2.png')
-    
-    # マリガン画面まで待機
-    print("Waiting for the confirm button to appear...")
-    while True:
-        try:
-            location = pyautogui.locateOnScreen('data/confirm_button.png', confidence=0.8)
-            if location:
-                break
-        except pyautogui.ImageNotFoundException:
-            pass
-        time.sleep(0.5)  # 短い間隔で確認
-    print("Confirm button appeared!")
-    
-    
-    # マリガンせずにコンシード
+
+def concede_match():
+    print('Condeding match...')
     pyautogui.press('esc')
     time.sleep(0.5)
     click_button_by_image('data/concede_button.png')
     time.sleep(10)
     pyautogui.click(100, 501) # 適当な場所をクリックしてdeck選択画面に戻る
+
+
+def play_vs_innkeeper():
+    start_match() # 試合開始
+    wait_for_image('data/confirm_button.png') # マリガン画面まで待機
+    concede_match()
+    wait_for_image('data/play_button.png') # デッキ選択画面まで待機
     
-    # 
-    while True:
-        try:
-            location = pyautogui.locateOnScreen('data/play_button.png', confidence=0.8)
-            if location:
-                break
-        except pyautogui.ImageNotFoundException:
-            pass
-        time.sleep(0.5)  # 短い間隔で確認 
-    
+
+def main():
+    for i in range(3):
+        play_vs_innkeeper()
 
         
 if __name__ == '__main__':
--- todo.md
+++ todo.md
@@ -1,9 +1,25 @@
 # todo
 
 
-## 自動化
-- [ ] マッチ待機時間のタイムアウトを設定する
-- [ ] wait_for_image, test_wait_for_image実装
+## マリガン
+- [ ] マリガンする関数mulligan()を定義する
+    - [ ] カード一枚をチェックする関数check_cardを定義する
+    - [ ] 手札のカードを保存するリストhandを定義する
+
+
+## ゲームの開始
+- [ ] 自分の手番かどうかを確認check_turn_button
+    - [ ] turn_button = greenの時はclickしてターンエンド->自分の手番になるのを待つ
+    - [ ] turn_button = yellowの時（自分のターンかつ、やれることがある）とき、プレイ
+
+    - [ ] turn_button = enemy_turnの時、待機
+
+
+- 手札の更新を行うとき
+    - ターンの最初
+    - ドローしたとき
+
+
 ## utilities
 - [ ] 複数ディスプレイに対応させる
 - [ ] 複数解像度に対応させる。
